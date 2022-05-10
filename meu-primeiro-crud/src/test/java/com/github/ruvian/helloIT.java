496716a51e02df014e7386c811d66d4a614bdace
Ruvian <ruvian.fabrin@hotmail.com>
2022-05-10 00:54:07 -0300

    First Commit

---
+++ meu-primeiro-crud/src/test/java/com/github/ruvian/helloIT.java
@@ -0,0 +1,9 @@
+package com.github.ruvian;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class helloIT extends helloTest {
+
+    // Execute the same tests but in native mode.
+}
